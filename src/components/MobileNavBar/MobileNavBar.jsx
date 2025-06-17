12641ed8cea89868bf03ea186f0bcd3c6fba2ea9
mpwow <mpwow@yandex.ru>
2025-06-17 20:54:05 +0300

    fix navbar

--- src/components/MobileNavBar/MobileNavBar.jsx
+++ src/components/MobileNavBar/MobileNavBar.jsx
@@ -3,8 +3,21 @@ import CauldronIcon from "../Header/ControlSection/CauldronIcon/CauldronIcon";
 import ProfileIcon from "../Header/ControlSection/ProfileIcon/ProfileIcon";
 import SearchIcon from "../Header/ControlSection/SearchIcon/SearchIcon";
 import ControlButton from "../Header/ControlSection/ControlButton/ControlButton";
+import { useSearch } from "../../context/SearchContext";
+import { useCart } from "../../context/CartContext";
 
 export default function ControlSectionMobile() {
+  const { cartItems, openCart } = useCart();
+  const { openSearchHandler } = useSearch();
+
+  const openCartHandler = () => {
+    openCart();
+  };
+
+  const openSearchInput = () => {
+    openSearchHandler();
+  };
+
   return (
     <nav
       className={styles.mobileNavBar}
@@ -12,16 +25,12 @@ export default function ControlSectionMobile() {
       <ControlButton
         Icon={SearchIcon}
         text={"Search"}
-        onClick={() => {
-          console.log("Clicked Search");
-        }}
+        onClick={openSearchInput}
       />
       <ControlButton
         Icon={CauldronIcon}
         text={"Cart"}
-        onClick={() => {
-          console.log("Clicked Cart");
-        }}
+        onClick={openCart}
       />
       <ControlButton
         Icon={ProfileIcon}
